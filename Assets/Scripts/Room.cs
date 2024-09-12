25ee39e7f5cfdf8113fe19b55730a31ae3915024
Diwar <diegogurrieri@live.com>
2024-09-12 11:28:01 +0200

    Fix camera controller

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public Transform roomCenter;
+    public Transform spawnPoint;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) 
+        if (other.CompareTag("Player"))
         {
+            other.transform.position = spawnPoint.position;
             Camera.main.GetComponent<CameraController>().MoveToRoom(roomCenter);
         }
     }
