917fd635e85065b5401b54319b56f357c2a2c8ad
Pietro <pietro.digiovanni.dev@gmail.com>
2024-09-13 08:59:51 +0200

    AI updated

--- Assets/Scripts/AI/Agent.cs
+++ Assets/Scripts/AI/Agent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Agent : MonoBehaviour
 {
@@ -29,33 +30,34 @@ public class Agent : MonoBehaviour
     }
     Vector2 desiredVelocity;
     Vector2 steeringVelocity;
+    private bool isFacingRight;
 
     private void FixedUpdate()
     {
-        /* RaycastHit2D rayHit = Physics2D.Raycast(transform.position, rb.velocity.normalized, warningRange, obstaclesLayer);
-         Debug.DrawRay(transform.position, rb.velocity.normalized * warningRange, Color.green);
-         if (rayHit)
-         {
-             Debug.Log("Collidendo con.. " + rayHit.transform);
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, rb.velocity.normalized, warningRange, obstaclesLayer);
+        Debug.DrawRay(transform.position, rb.velocity.normalized * warningRange, Color.green);
+        if (rayHit)
+        {
+            Debug.Log("Collidendo con.. " + rayHit.transform);
 
-             if (rayHit.transform != gameObject.transform)
-             {
-                 Debug.Log("Collidendo con.. " + rayHit.transform);
-                 if (Vector2.Distance(transform.position, rayHit.transform.position) < 2f)
-                 {
-                     desiredVelocity = (transform.position - (Vector3)rayHit.point).normalized;
-                     desiredVelocity *= maxVelocity;
+            if (rayHit.transform != gameObject.transform)
+            {
+                Debug.Log("Collidendo con.. " + rayHit.transform);
+                if (Vector2.Distance(transform.position, rayHit.transform.position) < 2f)
+                {
+                    desiredVelocity = (transform.position - (Vector3)rayHit.point).normalized;
+                    desiredVelocity *= maxVelocity;
 
-                     steeringVelocity = desiredVelocity - rb.velocity;
-                     steeringVelocity = Vector2.ClampMagnitude(steeringVelocity, maxForce);
+                    steeringVelocity = desiredVelocity - rb.velocity;
+                    steeringVelocity = Vector2.ClampMagnitude(steeringVelocity, maxForce);
 
-                     rb.velocity += steeringVelocity;
-                     rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+                    rb.velocity += steeringVelocity;
+                    rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
 
-                     transform.position += (Vector3)rb.velocity * Time.fixedDeltaTime;
-                 }
-             }
-         }*/
+                    transform.position += (Vector3)rb.velocity * Time.fixedDeltaTime;
+                }
+            }
+        }
 
         RaycastHit2D warningCircleHit = Physics2D.CircleCast(transform.position, warningRange, Vector2.right);
         if (warningCircleHit)
@@ -91,6 +93,8 @@ public class Agent : MonoBehaviour
             {
             }
         }
+
+        //HandleFlip(rb.velocity.normalized);
     }
 
     private Vector2 RandomTarget()
@@ -98,5 +102,26 @@ public class Agent : MonoBehaviour
         float randomX = Random.Range(areaMin.x, areaMax.x);
         float randomY = Random.Range(areaMin.y, areaMax.y);
         return new Vector2(randomX, randomY);
+    }
+
+    private void HandleFlip(Vector2 direction)
+    {
+        if (direction.x > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (direction.x < 0 && isFacingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
