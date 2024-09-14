f0e75c7f3dbf44072c4e162539e99f9f700888f5
Pietro <pietro.digiovanni.dev@gmail.com>
2024-09-15 00:52:27 +0200

    ui scripts

---
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
