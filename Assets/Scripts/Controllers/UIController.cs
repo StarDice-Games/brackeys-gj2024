04bf52b3ce7cf7ef11bbbe6692e4d31a736d827a
Diwar <diegogurrieri@live.com>
2024-09-15 10:35:30 +0200

    UI Multitask sounds

--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -10,8 +10,12 @@ public class UIController : MonoBehaviour
     [SerializeField] private List<TextMeshProUGUI> taskProgressTexts;
     [SerializeField] private TaskManager taskManager;
 
+    private bool[] taskCompleted;
+    [SerializeField] AudioClip taskAudioClip;
+
     private void Start()
     {
+        taskCompleted = new bool[taskProgressBars.Count];
         UpdateUI();
     }
 
@@ -30,6 +34,13 @@ public class UIController : MonoBehaviour
             float progress = task.GetCompletedObjectives() / (float)task.GetTotalObjectives();
             taskProgressBars[i].fillAmount = progress;
             taskProgressTexts[i].text = $"{task.GetCompletedObjectives()}/{task.GetTotalObjectives()}";
+
+            if (task.GetCompletedObjectives() == task.GetTotalObjectives() && !taskCompleted[i])
+            {
+                Debug.Log("Multitask completed: " + task.GetType().Name);
+                AudioController.Instance.PlaySound(taskAudioClip.name, true, "sfx");
+                taskCompleted[i] = true; 
+            }
 
             if (task is MultiTask multiTask)
             {
