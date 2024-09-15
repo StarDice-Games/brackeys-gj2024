40bc1890572dad26c4c73b5a9a907f0fd6975173
Pietro <pietro.digiovanni.dev@gmail.com>
2024-09-15 09:12:30 +0200

    Monster can't kill guests fixed

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -64,6 +64,8 @@ public class Item : MonoBehaviour, IInteractable
         {
             associatedTask.CheckCompletion();
         }
+
+        this.enabled = false;
     }
 
     private void InteractWithStaticObject()
