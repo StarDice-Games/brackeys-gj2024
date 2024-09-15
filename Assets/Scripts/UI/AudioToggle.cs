075c66a0e923357313ae43c815ef8842c8d27b31
Pietro <pietro.digiovanni.dev@gmail.com>
2024-09-15 11:21:23 +0200

    audio toggle icon fix

--- Assets/Scripts/UI/AudioToggle.cs
+++ Assets/Scripts/UI/AudioToggle.cs
@@ -10,11 +10,23 @@ public class AudioToggle : MonoBehaviour
     private void Awake()
     {
         toggle = GetComponent<Toggle>();
+
+        audioIcon.sprite = PlayerPrefs.GetInt("audioToggle", 1) == 1 ? audioOnIcon : audioOffIcon;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            PlayerPrefs.DeleteAll();
+        }
     }
 
     public void SetAudio()
     {
         audioIcon.sprite = toggle.isOn ? audioOnIcon : audioOffIcon;
+
+        PlayerPrefs.SetInt("audioToggle", toggle.isOn ? 1 : 0);
 
         if (toggle.isOn)
         {
